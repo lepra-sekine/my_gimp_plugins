c270e44791f2ad2fb29773a5a23288869383d7e7
SR <null>
2025-09-05 00:33:10 +0900

    fix GenerateDefaultGuids ダイアログが表示される不具合を修正

--- generate_default_guides.py
+++ generate_default_guides.py
@@ -4,32 +4,37 @@
 from gimpfu import *
 
 def generate_default_guides(image):
-    # 画像の幅と高さを取得
     i_w = pdb.gimp_image_width(image)
     i_h = pdb.gimp_image_height(image)
 
-    # ガイドを追加
-    pdb.gimp_image_add_vguide(image, 0)
-    pdb.gimp_image_add_hguide(image, 0)
-    pdb.gimp_image_add_vguide(image, int(i_w))
-    pdb.gimp_image_add_hguide(image, int(i_h))
-    pdb.gimp_image_add_vguide(image, int(i_w * 0.5))
-    pdb.gimp_image_add_hguide(image, int(i_h * 0.5))
+    # 縦ガイド
+    pdb.gimp_image_add_vguide(image, 0)              # 左端
+    pdb.gimp_image_add_vguide(image, i_w)            # 右端
+    pdb.gimp_image_add_vguide(image, int(i_w / 2))   # 中央
+
+    # 横ガイド
+    pdb.gimp_image_add_hguide(image, 0)              # 上端
+    pdb.gimp_image_add_hguide(image, i_h)            # 下端
+    pdb.gimp_image_add_hguide(image, int(i_h / 2))   # 中央
+
+    # 更新して画面に反映
+    pdb.gimp_displays_flush()
+
 
 register(
     "generate_default_guides",
-    "自動でガイドを生成します",
-    "選択レイヤーに関係なく、画像全体にガイドを追加します",
-    "作者名",
-    "作者名",
+    "画像の両端と中央にガイドを追加",
+    "画像全体の両端と中央に縦横のガイドを自動で作成します",
+    "SR",
+    "SR",
     "Mar 2025",
-    "<Image>/Filters/Custom/GenerateDefaultGuids",
-    "RGB*, GRAY*",   # 適用可能な画像タイプ
+    "<Image>/Filters/Custom/GenerateDefaultGuides",
+    "",
     [
         (PF_IMAGE, "image", "Input Image", None),
     ],
-    [], 
-    generate_default_guides
-)
+    [],
+    generate_default_guides,
+    menu="")
 
 main()
